14d095f9e6d71c1237b0d80d463d2a25018211b3
NG <nicholeguan2@gmail.com>
2022-10-28 00:16:46 -0700

    create api post route to receive a new note to save on req body and add it to db.json and return new note to client

--- server.js
+++ server.js
@@ -16,12 +16,52 @@ app.get('/api/notes', (req, res) => {
 
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) {
-        console.error(err);
-      } else {
-        res.json(JSON.parse(data));
-      }
+      console.error(err);
+    } else {
+      res.json(JSON.parse(data));
+    }
   })
   console.info(`${req.method} request received`);
+});
+
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to add a review`);
+
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+    };
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedNotes = JSON.parse(data);
+
+        parsedNotes.push(newNote);
+        notes = parsedNotes;
+
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr ? console.error(writeErr) : console.info('Successfully saved notes!')
+        );
+      }
+    });
+
+    const response = {
+      body: newNote,
+    };
+
+    res.json(response);
+  } 
+  else {
+    res.json('Error in saving note');
+  }
 });
 
 app.get('/notes', (req, res) =>
