b35d971f8bb20d47cad3c4bb4e7e577de965fa23
NG <nicholeguan2@gmail.com>
2022-10-27 23:48:27 -0700

    create api get route to read db.json return saved notes as JSON

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { appendFile } = require('fs');
 const path = require('path');
+const fs = require('fs');
+let notes = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,6 +12,18 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+      } else {
+        res.json(JSON.parse(data));
+      }
+  })
+  console.info(`${req.method} request received`);
+});
+
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
@@ -18,6 +31,7 @@ app.get('/notes', (req, res) =>
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
